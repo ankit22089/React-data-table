d48ec042cc0bb3b2b37cb0ba1eb39c7d094675c6
ankit3909 <64419882+ankit3909@users.noreply.github.com>
2021-05-26 13:14:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Tables from './Tables'
+
+function App() {
+  return (
+    <div className="App">
+    <Tables/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Tables.jsx
@@ -0,0 +1,319 @@
+import React from 'react'
+
+function Tables() {
+    return (
+        <div>
+            <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col-lg-2">Ticker</th>
+      <th scope="col">Date/Time</th>
+      <th scope="col">CMP</th>
+      <th scope="col">Price_Change%</th>
+      <th scope="col">Gap:52_W_H</th>
+      <th scope="col">Gap:52_W_L</th>
+      <th scope="col">St_1</th>
+      <th scope="col">St_2</th>
+      <th scope="col">Candle</th>
+      <th scope="col">Trend_</th>
+      <th scope="col">Score</th>
+      <th scope="col">Rank</th>
+      <th scope="col">MR_Signal</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">MOTHHERSUMI-I</th>
+      <td>17/05/21 9:15</td>
+      <td>241.05</td>
+      <td>5.26</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-2.05</b></td>
+      <td>-64.09</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>226.75</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>217.20</td>
+      <td>&nbsp;</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_52_W_H</td>
+      <td>140.49</td>
+      <td>19</td>
+      <td>&nbsp;</td>
+    </tr>
+
+    <tr>
+      <th scope="row">UPL-I</th>
+      <td>17/05/21 9:15</td>
+      <td>769.25</td>
+      <td>3.12</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>-2.21</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>-49.69</td> 
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>739.40</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>708.88</td>
+      <td></td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_52_W_H</td>
+      <td>261.84</td>
+      <td>3</td>
+      <td>&nbsp;</td>
+    </tr>
+
+<tr>
+      <th scope="row">BALKRISIND-I</th>
+      <td>17/05/21 9:15</td>
+      <td>2,101.00</td>
+      <td>11.18</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-0.43</b></td>
+      <td>-49.26</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>1921.34</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>1849.78</td>
+      <td>Up</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_52_W_H</td>
+      <td>305.68</td>
+      <td>1</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">BERGEPAINT-I</th>
+      <td>17/05/21 9:15</td>
+      <td>76930</td>
+      <td>2.16</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-6.77</b></td>
+      <td>-40.84</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>744.8</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>724.87</td>
+      <td></td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_50_D_H</td>
+      <td>94.41</td>
+      <td>40</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">COLPAL-I</th>
+      <td>17/05/21 9:15</td>
+      <td>1,611.00</td>
+      <td>2.35</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-3.99</b></td>
+      <td style={{backgroundColor:'rgb(250, 36, 36)'}}>18.91</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>1,571.73</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>1,537.56</td>
+      <td></td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_10_D_H</td>
+      <td>90.24</td>
+      <td>42</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">SBIN-I</th>
+      <td>17/05/21 9:15</td>
+      <td>386.20</td>
+      <td>6.83</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-10.92</b></td>
+      <td>-57.99</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>362.14</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>348.84</td>
+      <td>Up</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_10_D_H</td>
+      <td>80.15</td>
+      <td>47</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">ICICIBANK-I</th>
+      <td>17/05/21 9:15</td>
+      <td>624.50</td>
+      <td>4.42</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-9.77</b></td>
+      <td>-48.12</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>596.73</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>580.56</td>
+      <td>Up</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_10_D_H</td>
+      <td>76.70</td>
+      <td>50</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">UBL-I</th>
+      <td>17/05/21 9:15</td>
+      <td>1,234.85</td>
+      <td>1.87</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-7.34</b></td>
+      <td>-27.26</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>1,196.98</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>1,163.54</td>
+      <td></td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_10_D_H</td>
+      <td>61.90</td>
+      <td>58</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">ITC-I</th>
+      <td>17/05/21 9:15</td>
+      <td>213.20</td>
+      <td>-0.30</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-10.14</b></td>
+      <td>-23.45</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>210.30</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>205.95</td>
+      <td></td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_10_D_H</td>
+      <td>58.83</td>
+      <td>61</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">GMRINFRA-I</th>
+      <td>17/05/21 9:15</td>
+      <td>26.25</td>
+      <td>4.73</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-12.65</b></td>
+      <td>-33.14</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>24.78</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>23.87</td>
+      <td>Up</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_10_D_H</td>
+      <td>51.22</td>
+      <td>67</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">BRITABBIA-I</th>
+      <td>17/05/21 9:15</td>
+      <td>3,494.00</td>
+      <td>-0.46</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-12.95</b></td>
+      <td style={{backgroundColor:'rgb(250, 36, 36)'}}>6.10</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>3,444.95</td>
+      <td style={{backgroundColor:'rgb(250, 36, 36)'}}>1,530.23</td>
+      <td>Down</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_10_D_H</td>
+      <td>-38.23</td>
+      <td>119</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">EICHERMOT-I</th>
+      <td>17/05/21 9:15</td>
+      <td>2,495.05</td>
+      <td>2.86</td>
+      <td><b>-88.85</b></td>
+      <td  style={{backgroundColor:'rgb(250, 36, 36)'}}>13.35</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>2,403.67</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>2,339.37</td>
+      <td>Up</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_10_D_H</td>
+      <td>-34.72</td>
+      <td>118</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">RBLBANK-I</th>
+      <td>17/05/21 9:15</td>
+      <td>198.25</td>
+      <td>5.26</td>
+      <td><b>-27.58</b></td>
+      <td>-43.23</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>186.09</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>178.42</td>
+      <td>Up</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_10_D_H</td>
+      <td>-54.74</td>
+      <td>128</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">NESTLEIND-I</th>
+      <td>17/05/21 9:15</td>
+      <td>17,079.00</td>
+      <td>-1.28</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-9.37</b></td>
+      <td style={{backgroundColor:'rgb(250, 36, 36)'}}>11.50</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>16,889.13</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>16,582.63</td>
+      <td>Down</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_10_D_H</td>
+      <td>26.67</td>
+      <td>74</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">LT-I</th>
+      <td>17/05/21 9:15</td>
+      <td>1,391.40</td>
+      <td>-2.06</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-13.17</b></td>
+      <td>-39.42</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>1,372.02</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>1,339.44</td>
+      <td></td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_10_D_H</td>
+      <td>22.80</td>
+      <td>77</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">MUTHOOTFIN-I</th>
+      <td>17/05/21 9:15</td>
+      <td>1,233.10</td>
+      <td>5.48</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-12.20</b></td>
+      <td>-31.90</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>1,172.22</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>1,150.65</td>
+      <td>Up</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_10_D_H</td>
+      <td>18.06</td>
+      <td>79</td>
+      <td>&nbsp;</td>
+    </tr>
+
+
+<tr>
+      <th scope="row">TRENT-I</th>
+      <td>17/05/21 9:15</td>
+      <td>797.00</td>
+      <td>2.65</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}><b>-15.84</b></td>
+      <td style={{backgroundColor:'rgb(250, 36, 36)'}}>-13.43</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>759.47</td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>735.96</td>
+      <td></td>
+      <td style={{backgroundColor:'rgb(2, 177, 2)'}}>Buy_10_D_H</td>
+      <td></td>
+      <td>87</td>
+      <td>&nbsp;</td>
+    </tr>
+
+    
+        
+  </tbody>
+</table>
+        </div>
+    )
+}
+
+export default Tables
